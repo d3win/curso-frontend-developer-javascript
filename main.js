26f9c6f0856cd4666ad97d1f653a736ea75bacd3
d3win <d3win@hotmail.com>
2022-08-10 15:57:49 -0500

    ultima actualizacion

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click',toogleDesktopMenu);
+menuHamIcon.addEventListener('click',tooglemobileMenu);
+
+function tooglemobileMenu()
+{
+    mobileMenu.classList.toggle('inactive');
+}
 
 function toogleDesktopMenu(){
 desktopMenu.classList.toggle('inactive');
